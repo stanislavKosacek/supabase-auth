aa0f5d32d5901926fc23a582554229b0b50010f2
stanislavKosacek <44036206+stanislavKosacek@users.noreply.github.com>
2023-04-26 16:08:39 +0200

    remove url

--- src/functions/supabase.js
+++ src/functions/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://tgocafisnqldtwrlyort.supabase.co';
+const SUPABASE_URL = '';
 
 export const getSupabase = () => {
   return createClient(SUPABASE_URL, localStorage.getItem('key'));
